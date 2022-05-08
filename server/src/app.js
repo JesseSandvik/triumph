666b7c93517dccf7bcf4eb6fb96112ccb4f41f77
Jesse Sandvik <jsandvik89@gmail.com>
2022-05-08 02:39:28 -0400

    test added for notFound/404, created errorHandler and notFound functions for handling custom 404 and errorhandling

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
 const registerRouter = require("./register/register.router");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/register", registerRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
--- server/src/register/register.spec.js
+++ server/src/register/register.spec.js
@@ -3,11 +3,12 @@ const app = require("../app");
 
 describe("Register", () => {
   test("returns a 201 status for a successful POST request", async () => {
-    const newUser = {
-      username: "Dennis Reynolds",
-      password: "!Dayman10",
+    const user = {
+      username: "Dennis_Reynolds",
+      password: "!IamAGoldenGod10",
     };
-    const res = await request(app).post("/register").send({ data: newUser });
+
+    const res = await request(app).post("/register").send({ data: user });
 
     const { status } = res;
     const { data } = res.body;
@@ -17,8 +18,19 @@ describe("Register", () => {
     expect(status).toEqual(201);
     expect(res.type).toEqual(expect.stringContaining("json"));
     expect(data).toEqual(
-      expect.objectContaining({ username: newUser.username })
+      expect.objectContaining({
+        username: user.username,
+        password: expect.any(String),
+      })
     );
-    expect(password).not.toEqual(newUser.password);
+    expect(password).not.toEqual(user.password);
+  });
+  test("returns a 404 for a invalid/missing route", async () => {
+    const res = await request(app).post("/register/bad-route");
+
+    const { status } = res;
+    expect(res.body.error).toBeDefined();
+    expect(status).toEqual(404);
+    expect(res.body.error).toContain("/register/bad-route");
   });
 });
