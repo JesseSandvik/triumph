dc5bb2a2d9566f475eaa32b8e72d51e7f6f55a13
Jesse Sandvik <jsandvik89@gmail.com>
2022-05-10 18:16:08 -0400

    registration form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <form>
+        <label htmlFor='username'>Username: </label>
+        <input name='username' type='text' />
+        <label htmlFor='password'>Password: </label>
+        <input name='password' type='password' />
+        <label htmlFor='match'>Confirm Password: </label>
+        <input name='match' type='password' />
+        <button type='submit'>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
