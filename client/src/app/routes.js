2f111e06998143e7bd164f77cf21d21e2c2ad689
Jesse Sandvik <jsandvik89@gmail.com>
2022-05-21 13:29:10 -0400

    scss global file created with basic root resets, compiling script added to package.json for sass, confirmed proper functionality

--- client/src/app/routes.js
+++ client/src/app/routes.js
@@ -1,3 +1,4 @@
+import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Registration from "../pages/registration/Registration";
 
@@ -5,7 +6,7 @@ export const routes = [
   {
     name: "home",
     path: "/",
-    element: <Login />,
+    element: <Home />,
   },
   {
     name: "login",
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  <main>Home!</main>;
+};
+
+export default Home;
