2d0950203a73d197e4d36e958c2998da27f01dbe
Jesse Sandvik <jsandvik89@gmail.com>
2022-05-21 13:06:40 -0400

    react router dom 6 installed and initialized in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 
@@ -8,8 +9,10 @@ import { store } from "./app/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
