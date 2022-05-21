d7a7ac45cbd6a26bf50be69a9825f417605d7be6
Jesse Sandvik <jsandvik89@gmail.com>
2022-05-21 13:09:16 -0400

    routes and individual route components setup in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
 import Registration from "./components/pages/registration/Registration";
 
 const App = () => {
-  return <Registration />;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Registration />} />
+        <Route path='/register' element={<Registration />} />
+        <Route path='/login' element={<Registration />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
